2604277b27f7173063d5194ec6601deb17fea54b
tec <gilsonnetotec@gmail.com>
2023-07-01 06:34:21 -0300

    TASK-00-Aplication:Iniciando...

---
+++ application/src/main/java/com/delivery/system/products/application/deliveryproducts/DeliveryProductsApplication.java
@@ -0,0 +1,13 @@
+package com.delivery.system.products.application.deliveryproducts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryProductsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliveryProductsApplication.class, args);
+	}
+
+}
---
+++ application/src/test/java/com/delivery/system/products/application/deliveryproducts/DeliveryProductsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.delivery.system.products.application.deliveryproducts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryProductsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
