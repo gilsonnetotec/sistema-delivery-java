9d9cfc8c583baef891ea1bd15bf63b09bd29f204
tec <gilsonnetotec@gmail.com>
2023-07-01 13:11:08 -0300

    TASK-00-Aplication: "CategoryResource"

---
+++ application/src/main/java/com/delivery/system/products/application/deliveryproducts/resources/CategoryResource.java
@@ -0,0 +1,25 @@
+package com.delivery.system.products.application.deliveryproducts.resources;
+
+import com.delivery.system.products.application.deliveryproducts.entities.Category;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/categorias")
+public class CategoryResource {
+
+    @GetMapping
+    public ResponseEntity<List<Category>> findAll(){
+        List<Category> list = new ArrayList<>();
+        list.add(new Category(1L, "Books"));
+        list.add(new Category(2L, "Electonic"));
+
+        return ResponseEntity.ok().body(list);
+    }
+
+}
